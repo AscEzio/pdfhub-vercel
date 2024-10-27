af4f62fc33ae08c29a1bbdba8ce88a2378ac5ba3
AscEzio <1647638002@qq.com>
2024-10-27 21:30:49 +0800

    feat: 初始化应用路由

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import { RouterView } from 'oh-router-react'
+import { router } from './router'
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+  <RouterView router={router} />
 )
