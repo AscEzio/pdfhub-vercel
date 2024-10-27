149e48b6983664456fee73ea767c98866017f067
AscEzio <1647638002@qq.com>
2024-10-27 15:08:51 +0800

    chore: 配置vite选项

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { generateScopedName } from './scripts/generateScopedName'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }: UserConfig) => {
+  return defineConfig({
+    plugins: [
+      react()
+    ],
+    define: {
+      'process.env.NODE_ENV': `"${mode as string}"`
+    },
+    css: {
+      // 配置modules命名生成规则
+      modules: {
+        generateScopedName,
+        localsConvention: 'camelCase'
+      },
+      preprocessorOptions: {
+        less: {
+          math: 'always'
+        }
+      }
+    },
+    // 配置路径别名
+    resolve: {
+      alias: [
+        {
+          find: /^~/,
+          replacement: ''
+        },
+        { find: '@', replacement: path.resolve(__dirname, 'src') }
+      ]
+    },
+    base: './',
+    // 自定义本地服务端口
+    server: {
+      port: 3150
+    }
+  })
+}
